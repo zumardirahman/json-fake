b7ac01eb8234a2ca1e40014175556cdcafccb82b
zumardirahman <zumar2014@gmail.com>
2022-03-08 11:58:33 +0700

    update strutucre index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
-const router = _router('db.json'); // <== Will be created later
-const middlewares = defaults();
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
